fb7d26d7b1dfa46843cc9f05cd0c37269c6ea573
Puneetha Pai <puneethapai29@gmail.com>
2020-09-28 13:59:42 +0530

    Add basic structure to create python packag

---
+++ feature_selection_ga/__init__.py
@@ -0,0 +1,6 @@
+__author__ = """Kaushal Shetty"""
+__email__ = "kaushalshetty@outlook.com"
+__version__ = "0.1.0"
+
+from .feature_selection_ga import FeatureSelectionGA
+from .fitness_function import FitnessFunction
