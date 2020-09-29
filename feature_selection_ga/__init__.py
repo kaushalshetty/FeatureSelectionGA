c3130f64d9c6221afbe9b1d1a20fbc8a1437346b
Puneetha Pai <puneethapai29@gmail.com>
2020-09-29 10:29:57 +0530

    Bump version: 0.1.1 → 0.1.2

--- feature_selection_ga/__init__.py
+++ feature_selection_ga/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Kaushal Shetty"""
 __email__ = "kaushalshetty@outlook.com"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .feature_selection_ga import FeatureSelectionGA
 from .fitness_function import FitnessFunction
